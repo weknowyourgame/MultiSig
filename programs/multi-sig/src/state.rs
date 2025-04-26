67674c06b853e5a2a81394e2e920490cf5a485f7
Sarthak2 <aniki123.ai@gmail.com>
2025-04-27 04:44:00 +0530

    added state

---
+++ programs/multi-sig/src/state.rs
@@ -0,0 +1,61 @@
+use anchor_lang::prelude::*
+
+#[account]
+pub struct MultiSigAccount {
+    pub owners: Vec<Pubkey>,
+    pub threshold: u8,
+    pub nonce: u8  
+}
+
+#[account]
+pub struct Transactions {
+    pub transaction_index: u64,
+    pub parent: Pubkey,
+    pub initiator: Pubkey,
+    pub accounts: Vec<SignerAccount>,
+    pub signers: Vec<bool>,
+    pub time: i64,
+    pub data: Vec<u8>,
+    pub did_complete: bool
+}
+
+pub struct SignerAccount {
+    pub pubkey: Pubkey,
+    pub is_signer: bool
+}
+
+impl MultiSigAccount {
+    pub fn get_max_size(num_owners: usize) -> usize {
+        // Base size
+        let base_size = 1 +  // threshold (u8)
+                        1;   // nonce (u8)
+        
+        // Vector of owners
+        let owners_size = 4 +         // vec length prefix
+                         (32 * num_owners);
+        
+        base_size + owners_size
+    }
+}
+
+impl Transactions {
+    pub fn get_max_size(num_signers: usize) -> usize {
+        // Base size
+        let base_size = 8 +   // transaction_index (u64)
+                        32 +  // parent (Pubkey)
+                        32 +  // initiator (Pubkey)
+                        8 +   // time (i64)
+                        1;    // did_complete (bool)
+        
+        let accounts_size = 4 +                              // vec length prefix
+                           (num_signers * (32 + 1));         // accounts max * (pubkey + 1 bool)
+        
+        let signers_size = 4 +                     // vec length prefix
+                          num_signers;             // booleans (1 per owner)
+
+        let data_size = 4 +                        // vec length prefix
+                       1024;                       // assume max instruction data size of 1KB
+        
+        base_size + accounts_size + signers_size + data_size
+    }
+}
